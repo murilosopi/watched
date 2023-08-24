95188b47a9fb9d19615d0446f0d45e0845b29d1f
Murilo Sopi <dev.murilosopi@gmail.com>
2023-08-24 19:18:25 -0300

    altera estrutura de diretórios para views

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="home">
+    <Slider />
     
   </div>
 </template>
 
 <script>
+import Slider from '@/components/Slider.vue'
 export default {
-
+  components: {
+    Slider
+  }
 }
 </script>
 
--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,12 @@ import Router from 'vue-router'
 Vue.use(Router);
 
 
-import Home from '@/components/pages/Home';
-import Login from '@/components/pages/Login';
-import SignUp from '@/components/pages/SignUp';
-import Search from '@/components/pages/Search';
-import Profile from '@/components/pages/Profile';
-import Movie from '@/components/pages/Movie';
+import Home from '@/pages/Home';
+import Login from '@/pages/Login';
+import SignUp from '@/pages/SignUp';
+import Search from '@/pages/Search';
+import Profile from '@/pages/Profile';
+import Movie from '@/pages/Movie';
 
 export default new Router({
   mode: 'history',
