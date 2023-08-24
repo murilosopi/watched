b638e99ae21c39b98adeca74f6c1ffb07f3b894f
Murilo Sopi <dev.murilosopi@gmail.com>
2023-08-24 20:29:00 -0300

    configura response headers em Action->retornarResposta

--- api/App/Action.php
+++ api/App/Action.php
@@ -3,6 +3,9 @@ namespace App;
 
 class Action {
     protected function retornarResposta(array|object $dados) {
+        header('Content-Type: application/json');
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Headers: *");
         $json = json_encode($dados, JSON_UNESCAPED_UNICODE);
 
         if($json === null) $this->retornarErro();
