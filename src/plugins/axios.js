c1384c0e19c6343ef9673b8cfcaa4715bca01b83
Murilo Sopi <dev.murilosopi@gmail.com>
2023-08-26 18:55:14 -0300

    altera Content-Type via interceptor do Axios

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -4,7 +4,15 @@ import Axios from 'axios'
 Vue.use({
   install(Vue) {
     Vue.prototype.$api = Axios.create({
-      baseURL: 'http://localhost'
+      baseURL: 'http://localhost',
+    });
+
+    Vue.prototype.$api.interceptors.request.use(config => {
+      if(config.method == 'post') {
+        config.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+      }
+
+      return config;
     });
   }
 });
