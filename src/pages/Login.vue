b5ce9d54b4c82476dada400bb0be58db6a3c4ead
Murilo Sopi <dev.murilosopi@gmail.com>
2023-08-27 10:21:25 -0300

    adiciona botões à página de Login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -14,37 +14,37 @@
       </p>
     </div>
 
-
-    <div class="row justify-content-center">
-      <div class="col-md-10 col-lg-7 col-xl-5">
-        <InputCustom>
-          <i class="bi bi-person-circle" slot="icon"></i>
-          <input type="text" placeholder="Usuário" slot="input" class="form-control">
-        </InputCustom>
-      </div>
-    </div>
-    <div class="row justify-content-center">
-      <div class="col-md-10 col-lg-7 col-xl-5">
-        <InputCustom>
-          <i class="bi bi-lock-fill" slot="icon"></i>
-          <input type="password" placeholder="Senha" slot="input" class="form-control">
-        </InputCustom>
-      </div>
-    </div>
-
-    <div class="row justify-content-center">
-      <div class="col-md-10 col-lg-7 col-xl-5">
-        <div class="d-flex">
-          <ButtonCustom class="me-2" @click.native="voltar">
-            Voltar
-          </ButtonCustom>
-          <ButtonCustom variant="azul" class="ms-2">
-            Entrar
-          </ButtonCustom>
+          
+      <div class="row justify-content-center">
+        <div class="col-md-10 col-lg-7 col-xl-5">
+          <InputCustom>
+            <i class="bi bi-person-circle" slot="icon"></i>
+            <input type="text" placeholder="Usuário" slot="input" class="form-control">
+          </InputCustom>
         </div>
       </div>
-    </div>
+      <div class="row justify-content-center">
+        <div class="col-md-10 col-lg-7 col-xl-5">
+          <InputCustom>
+            <i class="bi bi-lock-fill" slot="icon"></i>
+            <input type="password" placeholder="Senha" slot="input" class="form-control">
+          </InputCustom>
+        </div>
+      </div>
 
+      <div class="row justify-content-center">
+        <div class="col-md-10 col-lg-7 col-xl-5">
+          <div class="d-flex">
+            <ButtonCustom class="me-2" @click.native="voltar">
+              Voltar
+            </ButtonCustom>
+            <ButtonCustom variant="azul" class="ms-2">
+              Entrar
+            </ButtonCustom>
+          </div>
+        </div>
+      </div>
+    
   </div>
 </template>
 
