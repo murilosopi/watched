bc29c2fb49f5174796a5fbdfad9f52fdbabd02ce
Murilo Sopi <dev.murilosopi@gmail.com>
2023-08-29 17:57:35 -0300

    cria componente DarkBox

---
+++ src/components/DarkBox.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="dark-box">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.dark-box {
+  background-color: rgb(23, 23, 23);
+}
+</style>
