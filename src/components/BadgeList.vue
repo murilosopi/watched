60bbe446b9163f495c80a7188050989b9a4e8c1e
Murilo Sopi <dev.murilosopi@gmail.com>
2023-08-29 18:18:36 -0300

    cria componentes CustomBadge e BadgeList

---
+++ src/components/BadgeList.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul class="d-flex gap-2 list-unstyled mb-4 flex-wrap">
+    <CustomBadge tag="li" v-for="badge in badges" :key="badge.text">
+      <router-link v-if="badge.to" :to="badge.to">
+        {{ badge.text }}
+      </router-link>
+      <template v-else>
+        {{ badge.text }}
+      </template>
+    </CustomBadge>
+  </ul>
+</template>
+
+<script>
+import CustomBadge from "@/components/CustomBadge.vue";
+export default {
+  props: {
+    badges: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  components: {
+    CustomBadge,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/CustomBadge.vue
@@ -0,0 +1,20 @@
+<template>
+  <component :is="tag" class="badge rounded-pill border px-3 py-2">
+    <slot></slot>
+  </component>
+</template>
+
+<script>
+export default {
+  props: {
+    tag: {
+      type: String,
+      default: 'span'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
