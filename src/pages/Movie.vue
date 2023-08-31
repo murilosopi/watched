c53fe4c30f0627f85241c4d405d8573782977238
Murilo Sopi <dev.murilosopi@gmail.com>
2023-08-30 21:17:17 -0300

    adiciona busca de informações do filme em Movie.vue

--- src/pages/Movie.vue
+++ src/pages/Movie.vue
@@ -4,27 +4,19 @@
       <!-- Cartaz do filme -->
       <div class="col-3 d-none d-md-block">
         <MoviePoster
-          :url="'https://s2.glbimg.com/Zo7GePN37wWm8PSsRQDuWVyJSgo=/e.glbimg.com/og/ed/f/original/2019/03/25/spiderffh_tsrposter_london_breve.jpg'"
-          :title="'lorem'"
+          :url="movie.cartaz"
+          :title="movie.titulo"
           tag="figure"
           class="m-auto"
         ></MoviePoster>
       </div>
-
       <!-- Principais informações do filme -->
       <div class="col order-1 order-md-0">
         <div class="col p-0">
-          <h2 class="h1 title">Lorem ipsum dolar</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed veniam
-            praesentium porro qui unde dignissimos repudiandae, voluptatum,
-            corrupti sequi illum adipisci, iure ducimus placeat maiores et aut
-            nulla hic aliquam?
-          </p>
-
+          <h2 class="h1 title">{{ movie.titulo }}</h2>
+          <p>{{ movie.sinopse }}</p>
           <!-- Listagem de gêneros -->
-          <BadgeList :badges="[]"/>
-
+          <BadgeList :badges="[]" />
           <div class="d-flex">
             <!-- Contagem de estrelas pela avaliação média -->
             <ul class="list-unstyled d-inline-flex gap-1 me-2">
@@ -34,19 +26,16 @@
               <li><i class="bi bi-star-half"></i></li>
               <li><i class="bi bi-star"></i></li>
             </ul>
-
             <!-- Avaliação média -->
-            <p class="me-auto me-sm-4">5.0</p>
-
+            <p class="me-auto me-sm-4">{{ movie.nota }}</p>
             <!-- Duração -->
             <p>
               <i class="bi bi-clock"></i>
-              2h10min
+              {{ movie.duracaoMin | duracao }}
             </p>
           </div>
         </div>
       </div>
-
       <div class="order-sm-0 order-1 col-sm-2 ps-5 p-md-0 mt-2 mt-sm-0">
         <div class="d-flex flex-wrap justify-content-end gap-3">
           <!-- Curtir/descurtir -->
@@ -54,13 +43,11 @@
             <i class="bi bi-heart"></i>
             0
           </IconButton>
-
           <!-- Marcar/desmarcar assistido -->
           <IconButton>
             <i class="bi bi-camera-reels"></i>
             0
           </IconButton>
-
           <!-- salvar/removerSalvoFilme -->
           <IconButton>
             <i class="bi bi-bookmark-star"></i>
@@ -69,6 +56,7 @@
         </div>
       </div>
     </DarkBox>
+    <MovieDetails :movie="movie" />
   </div>
 </template>
 
@@ -78,17 +66,70 @@ import IconButton from "@/components/IconButton.vue";
 import PageMixin from "@/mixins/PageMixin";
 import DarkBox from "@/components/DarkBox.vue";
 import BadgeList from "@/components/BadgeList.vue";
+import MovieDetails from "@/components/MovieDetails.vue";
 export default {
   components: {
     MoviePoster,
     IconButton,
     DarkBox,
-    BadgeList
+    BadgeList,
+    MovieDetails,
   },
   mixins: [PageMixin],
+  filters: {
+    duracao(valor) {
+      if (valor < 60) {
+        return `${valor}min`;
+      }
+
+      let horas = parseInt(valor / 60);
+      let minutos = valor % 60;
+
+      return `${horas}h${minutos}min`;
+    },
+  },
+  props: ["id"],
+  data() {
+    return {
+      movie: {},
+    };
+  },
   created() {
     this.alterarFavicon("filme", "svg");
     this.alterarTitle("Filme");
+    this.obterDetalhesFilme();
+    this.obterNotaFilme();
+  },
+  methods: {
+    obterDetalhesFilme() {
+      const params = { id: this.id };
+      this.$api
+        .get("/detalhes-filme", { params })
+        .then((res) => {
+          const response = res.data;
+
+          if (response.sucesso) {
+            this.movie = { ...response.dados, nota: this.movie.nota };
+          }
+
+          this.alterarTitle(this.movie.titulo);
+        })
+        .catch(() => {});
+    },
+
+    obterNotaFilme() {
+      const params = { id: this.id };
+      this.$api
+        .get("/obter-nota-filme", { params })
+        .then((res) => {
+          const response = res.data;
+
+          if (response.sucesso) {
+            this.movie.nota = Number(response.dados.nota).toFixed(1);
+          }
+        })
+        .catch(() => {});
+    },
   },
 };
 </script>
