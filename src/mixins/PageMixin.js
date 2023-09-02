e87403a2ac1d2a77f19a6f27675f3ac39360deb4
Murilo Sopi <dev.murilosopi@gmail.com>
2023-09-02 10:18:05 -0300

    diretório de imagens

--- src/mixins/PageMixin.js
+++ src/mixins/PageMixin.js
@@ -2,7 +2,7 @@ export default {
   methods: {
     changeFavicon(icone, tipo) { 
       const favicon = document.querySelector("link[rel~='icon']");
-      favicon.href = `/favicons/${icone}.${tipo}`;
+      favicon.href = `/assets/img/favicons/${icone}.${tipo}`;
     },
     changePageTitle(title = '') {
       const el = document.querySelector("title");
