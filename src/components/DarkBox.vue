5c0f0b92fd7b63e372529993e39b84a4e46a81fd
Murilo Sopi <dev.murilosopi@gmail.com>
2023-09-02 13:56:11 -0300

    adiciona variação "gray" p o componente DarkBox

--- src/components/DarkBox.vue
+++ src/components/DarkBox.vue
@@ -1,17 +1,23 @@
 <template>
-  <div class="dark-box">
+  <div class="dark-box" :class="`${variant}`">
     <slot></slot>
   </div>
 </template>
 
 <script>
 export default {
-
+  props: {
+    variant: String
+  }
 }
 </script>
 
 <style>
 .dark-box {
-  background-color: rgb(23, 23, 23);
+  background: rgba(23, 23, 23, 0.90);
+}
+
+.dark-box.gray {
+  background-color: rgba(217, 217, 217, 0.05);
 }
 </style>
