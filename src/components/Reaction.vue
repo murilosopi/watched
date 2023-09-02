10c68c3be5fff396f8bb009125dd90a0b2142b3e
Murilo Sopi <dev.murilosopi@gmail.com>
2023-09-02 19:10:13 -0300

    cria componentes Reaction e Review

---
+++ src/components/Reaction.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="row justify-content-center align-items-center h-75">
+    <div class="col-lg-4 col-3">
+      <InteractiveIcon class="mx-auto" tag="label" for="smile">
+        <i class="bi fs-2 bi-emoji-smile"></i>
+        Satisfeito
+      </InteractiveIcon>
+    </div>
+
+    <div class="col-lg-4 col-3">
+      <InteractiveIcon class="mx-auto" tag="label" for="laughing">
+        <i class="bi fs-2 bi-emoji-laughing"></i>
+        Animado
+      </InteractiveIcon>
+    </div>
+
+    <div class="col-lg-4 col-3">
+      <InteractiveIcon class="mx-auto" tag="label" for="heart-eyes">
+        <i class="bi fs-2 bi-emoji-heart-eyes"></i>
+        Amei
+      </InteractiveIcon>
+    </div>
+
+    <div class="col-lg-4 col-3">
+      <InteractiveIcon class="mx-auto" tag="label" for="dizzy">
+        <i class="bi fs-2 bi-emoji-dizzy"></i>
+        Abismado
+      </InteractiveIcon>
+    </div>
+
+    <div class="col-lg-4 col-3">
+      <InteractiveIcon class="mx-auto" tag="label" for="neutral">
+        <i class="bi fs-2 bi-emoji-neutral"></i>
+        Tedioso
+      </InteractiveIcon>
+    </div>
+
+    <div class="col-lg-4 col-3">
+      <InteractiveIcon class="mx-auto" tag="label" for="frown">
+        <i class="bi fs-2 bi-emoji-frown"></i>
+        Triste
+      </InteractiveIcon>
+    </div>
+
+
+    <input @change="changeReactions" type="checkbox" v-model="reactions" id="smile" value="smile" hidden>
+    <input @change="changeReactions" type="checkbox" v-model="reactions" id="laughing" value="laughing" hidden>
+    <input @change="changeReactions" type="checkbox" v-model="reactions" id="heart-eyes" value="heart-eyes" hidden>
+    <input @change="changeReactions" type="checkbox" v-model="reactions" id="dizzy" value="dizzy" hidden>
+    <input @change="changeReactions" type="checkbox" v-model="reactions" id="neutral" value="neutral" hidden>
+    <input @change="changeReactions" type="checkbox" v-model="reactions" id="frown" value="frown" hidden>
+  </div>
+</template>
+
+<script>
+import InteractiveIcon from './InteractiveIcon.vue';
+export default {
+  props: ['value'],
+  data() {
+    return {
+      reactions: [...this.value]
+    }
+  },
+  watch: {
+    reactions() {
+      this.$emit('change', this.reactions);
+    }
+  },
+  methods: {
+    changeReactions(event) {
+      const el = event.target;
+      const icon = document.querySelector(`label[for="${el.id}"] i`);
+
+      if(!el.checked) {
+        icon.className = icon.className.replace('-fill', '')
+      } else if(this.reactions.length <= 3) {
+        icon.className = `${icon.className}-fill`;
+      } else {
+        this.reactions.pop();
+      }
+    }
+  },
+  components: {
+    InteractiveIcon
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/pages/movie/Review.vue
@@ -0,0 +1,71 @@
+<template>
+  <section class="col">
+    <Title tag="h3" class="text-center d-none">
+      <i class="bi bi-chat-left-text"></i>&nbsp; Que tal compartilhar abaixo a
+      sua opinião? :)
+    </Title>
+    <hr class="w-25 mx-auto" />
+    <DarkBox variant="gray" class="row px-md-5 px-3 py-5">
+      <form @submit.prevent="submitReview">
+        <div class="row">
+          <fieldset class="col d-flex">
+            <p class="me-3">Avalie o filme:</p>
+            <StarRating
+              :edit="true"
+              v-model="review.rating"
+              @change="review.rating = $event"
+            />
+          </fieldset>
+        </div>
+        <div class="row text-center justify-content-center">
+          <div class="col-lg order-lg-1">
+            <InputCustom :icon="false">
+              <textarea
+                v-model="review.text"
+                slot="input"
+                rows="10"
+                class="form-control fs-6"
+                placeholder="Conta pra gente como foi o filme :)"
+              ></textarea>
+            </InputCustom>
+          </div>
+          <div class="col-lg-5 order-lg-0">
+            <Reaction
+              v-model="review.reactions"
+              @change="review.reactions = $event"
+            />
+          </div>
+        </div>
+      </form>
+    </DarkBox>
+  </section>
+</template>
+
+<script>
+import Title from "@/components/Title.vue";
+import StarRating from "@/components/StarRating.vue";
+import InputCustom from "@/components/InputCustom.vue";
+import Reaction from "@/components/Reaction.vue";
+import DarkBox from "@/components/DarkBox.vue";
+
+export default {
+  components: {
+    Title,
+    StarRating,
+    InputCustom,
+    Reaction,
+    DarkBox
+  },
+  data() {
+    return {
+      review: {
+        rating: 0,
+        reactions: []
+      },
+    }
+  }
+};
+</script>
+
+<style>
+</style>
