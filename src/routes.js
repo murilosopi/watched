4abac19393557e560e1cded792a6dcfd5bd645b9
Murilo Sopi <dev.murilosopi@gmail.com>
2023-09-02 19:09:41 -0300

    corrige caminho de páginas em routes.js

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,12 @@ import Router from 'vue-router'
 Vue.use(Router);
 
 
-import Home from '@/pages/Home';
-import Login from '@/pages/Login';
-import SignUp from '@/pages/SignUp';
-import Search from '@/pages/Search';
-import Profile from '@/pages/Profile';
-import Movie from '@/pages/Movie';
+import Home from '@/pages/home/Home';
+import Login from '@/pages/user/Login';
+import SignUp from '@/pages/user/SignUp';
+import Search from '@/pages/search/Search';
+import Profile from '@/pages/user/Profile';
+import Movie from '@/pages/movie/Movie';
 
 export default new Router({
   mode: 'history',
