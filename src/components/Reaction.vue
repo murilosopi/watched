429dd2851af00e43664fdd9505c1e8db8e2630ef
Murilo Sopi <dev.murilosopi@gmail.com>
2023-09-03 12:53:09 -0300

    refatora componente Reaction

--- src/components/Reaction.vue
+++ src/components/Reaction.vue
@@ -1,88 +1,67 @@
 <template>
   <div class="row justify-content-center align-items-center">
-    <div class="col-lg-4 col-3">
-      <InteractiveIcon class="mx-auto" tag="label" for="smile">
-        <i class="bi fs-2 bi-emoji-smile"></i>
-        Satisfeito
+    <div class="col-lg-4 col-3" v-for="emoji in emojis" :key="emoji.value">
+      <InteractiveIcon class="mx-auto" tag="label" :for="emoji.value">
+        <i
+          class="bi fs-2"
+          :class="
+            isReactionChecked(emoji.value) ? `${emoji.icon}-fill` : emoji.icon
+          "
+        ></i>
+        {{ emoji.value | capitalize }}
       </InteractiveIcon>
+
+      <input
+        type="checkbox"
+        v-model="reactions"
+        :id="emoji.value"
+        :value="emoji.value"
+        hidden
+        @change="updateReactions"
+        :checked="isReactionChecked(emoji.value)"
+      />
     </div>
-
-    <div class="col-lg-4 col-3">
-      <InteractiveIcon class="mx-auto" tag="label" for="laughing">
-        <i class="bi fs-2 bi-emoji-laughing"></i>
-        Animado
-      </InteractiveIcon>
-    </div>
-
-    <div class="col-lg-4 col-3">
-      <InteractiveIcon class="mx-auto" tag="label" for="heart-eyes">
-        <i class="bi fs-2 bi-emoji-heart-eyes"></i>
-        Amei
-      </InteractiveIcon>
-    </div>
-
-    <div class="col-lg-4 col-3">
-      <InteractiveIcon class="mx-auto" tag="label" for="dizzy">
-        <i class="bi fs-2 bi-emoji-dizzy"></i>
-        Abismado
-      </InteractiveIcon>
-    </div>
-
-    <div class="col-lg-4 col-3">
-      <InteractiveIcon class="mx-auto" tag="label" for="neutral">
-        <i class="bi fs-2 bi-emoji-neutral"></i>
-        Tedioso
-      </InteractiveIcon>
-    </div>
-
-    <div class="col-lg-4 col-3">
-      <InteractiveIcon class="mx-auto" tag="label" for="frown">
-        <i class="bi fs-2 bi-emoji-frown"></i>
-        Triste
-      </InteractiveIcon>
-    </div>
-
-
-    <input @change="changeReactions" type="checkbox" v-model="reactions" id="smile" value="smile" hidden>
-    <input @change="changeReactions" type="checkbox" v-model="reactions" id="laughing" value="laughing" hidden>
-    <input @change="changeReactions" type="checkbox" v-model="reactions" id="heart-eyes" value="heart-eyes" hidden>
-    <input @change="changeReactions" type="checkbox" v-model="reactions" id="dizzy" value="dizzy" hidden>
-    <input @change="changeReactions" type="checkbox" v-model="reactions" id="neutral" value="neutral" hidden>
-    <input @change="changeReactions" type="checkbox" v-model="reactions" id="frown" value="frown" hidden>
   </div>
 </template>
 
 <script>
-import InteractiveIcon from './InteractiveIcon.vue';
+import InteractiveIcon from "./InteractiveIcon.vue";
 export default {
-  props: ['value'],
+  props: ["value"],
+  filters: {
+    capitalize(name) {
+      const first = name.charAt(0);
+      return name.replace(first, first.toUpperCase());
+    },
+  },
   data() {
     return {
-      reactions: [...this.value]
-    }
-  },
-  watch: {
-    reactions() {
-      this.$emit('change', this.reactions);
-    }
+      reactions: this.value,
+      emojis: [
+        { icon: "bi-emoji-frown", value: "triste" },
+        { icon: "bi-emoji-neutral", value: "tedioso" },
+        { icon: "bi-emoji-smile", value: "satisfeito" },
+        { icon: "bi-emoji-laughing", value: "animado" },
+        { icon: "bi-emoji-dizzy", value: "amei" },
+        { icon: "bi-emoji-heart-eyes", value: "abismado" },
+      ],
+    };
   },
   methods: {
-    changeReactions(event) {
-      const el = event.target;
-      const icon = document.querySelector(`label[for="${el.id}"] i`);
+    updateReactions() {
+      this.$emit("change", this.reactions);
+    },
 
-      if(!el.checked) {
-        icon.className = icon.className.replace('-fill', '')
-      } else if(this.reactions.length <= 3) {
-        icon.className = `${icon.className}-fill`;
-      } else {
-        this.reactions.pop();
-      }
-    }
+    isReactionChecked(reaction) {
+      return this.value.some((r) => r == reaction);
+    },
   },
   components: {
-    InteractiveIcon
-  }
+    InteractiveIcon,
+  },
+  updated() {
+    this.reactions = this.value;
+  },
 };
 </script>
 
