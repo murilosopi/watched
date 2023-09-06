ef186b405e0da5d1bd583bf3019fea9ed6b40d8a
Murilo Sopi <dev.murilosopi@gmail.com>
2023-09-06 14:34:22 -0300

    cria componentes UserAvatar e UserStats

---
+++ src/components/UserAvatar.vue
@@ -0,0 +1,49 @@
+<template>
+  <figure class="m-auto">
+    <img :src="path" :alt="alt" class="user-photo" :class="{ 'placeholder': !alreadyLoaded }">
+  </figure>
+</template>
+
+<script>
+export default {
+  props: {
+    username: {
+      required: true
+    }
+  },
+
+  data() {
+    return {
+      path: false,
+      alt: `Foto de perfil do usuário @${this.username}`
+    }
+  },
+
+  computed: {
+    alreadyLoaded() {
+      return this.path;
+    }
+  },
+
+  methods: {
+    getAvatarImage() {
+      setTimeout(() => {
+        this.path = '';
+      }, 2000 );
+    }
+  },
+
+  created() {
+    this.getAvatarImage();
+  }
+}
+</script>
+
+<style scoped>
+.user-photo {
+  aspect-ratio: 1 / 1;
+  object-fit: cover;
+  border-radius: 50%;
+  width: 100%;
+}
+</style>
---
+++ src/pages/user/UserStats.vue
@@ -0,0 +1,43 @@
+<template>
+  <ul class="user-stats col-lg-3 d-none d-lg-flex flex-column m-auto gap-2">
+    <li class="d-flex align-items-center gap-2" :class="{ 'placeholder placeholder-sm': !alreadyLoaded }">
+      <i class="bi bi-people-fill fs-5"></i>
+      <strong>Seguidores:</strong> {{ 0 }}
+    </li>
+    <li class="d-flex align-items-center gap-2" :class="{ 'placeholder placeholder-sm': !alreadyLoaded }">
+      <i class="bi bi-person-check-fill fs-5"></i>
+      <strong>Seguindo:</strong> {{ 0 }}
+    </li>
+    <li class="d-flex align-items-center gap-2" :class="{ 'placeholder placeholder-sm': !alreadyLoaded }">
+      <i class="bi bi-film fs-5"></i>
+      <strong>Assistidos:</strong> {{ 0 }}
+    </li>
+    <li class="d-flex align-items-center gap-2" :class="{ 'placeholder placeholder-sm': !alreadyLoaded }">
+      <i class="bi bi-chat-left-quote fs-5"></i>
+      <strong>Resenhas:</strong> {{ 0 }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    username: { required: true }
+  },
+
+  data() {
+    return {
+      stats: {}
+    }
+  },
+
+  computed: {
+    alreadyLoaded() {
+      return Object.keys(this.stats).length > 0;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
