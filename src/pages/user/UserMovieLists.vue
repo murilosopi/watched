ac7f462d9f00f222778d511edd986bf87f63111d
Murilo Sopi <dev.murilosopi@gmail.com>
2023-09-15 13:49:25 -0300

    cria componente UserMovieLists

---
+++ src/pages/user/UserMovieLists.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="user-movie-lists">
+    <MovieListSection
+      v-for="(list, idx) in interactionList"
+      :key="list.name"
+      :name="idx"
+      :movies="list.movies"
+    >
+      <i
+        :class="{
+          'bi-heart': idx == 'liked',
+          'bi-camera-reels': idx == 'watched',
+          'bi-bookmark-star': idx == 'saved',
+        }"
+        class="bi me-2 fs-5"
+        slot="icon"
+      ></i>
+    </MovieListSection>
+
+    <MovieListSection
+      v-for="(list, idx) in lists"
+      :key="list.id"
+      :name="list.name"
+      :movies="list.movies"
+      :class="idx + 1 == list.length ? 'mb-3' : ''"
+    >
+      <i class="bi bi-heart me-2 fs-5" slot="icon"></i>
+    </MovieListSection>
+  </div>
+</template>
+
+<script>
+import MovieListSection from "@/components/MovieListSection.vue";
+
+export default {
+  props: ["uid", "lists"],
+  data() {
+    return {
+      interactionList: {},
+    };
+  },
+  components: {
+    MovieListSection,
+  },
+  methods: {
+    getInteractionLists() {
+      // request to api
+      this.interactionList.liked = [];
+      this.interactionList.watched = [];
+      this.interactionList.saved = [];
+    },
+  },
+  created() {},
+};
+</script>
+
+<style></style>
