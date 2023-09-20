49d125a206857b7611132c4a728a26a3bf08c54d
Murilo Sopi <dev.murilosopi@gmail.com>
2023-09-20 06:23:45 -0300

    altera nome da prop passada p Comment.vue

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -1,31 +1,31 @@
 <template>
   <article class="comment">
-    <div class="row align-items-center">
+    <div class="row align-items-center mb-2">
       <figure class="col-1 p-0 mb-0">
-        <img class="user-photo w-100" src="/assets/img/users/default.svg" :alt="`Foto de perfil de @${review.user}`">
+        <img class="user-photo w-100" src="/assets/img/users/default.svg" :alt="`Foto de perfil de @${comment.user}`">
       </figure>
 
       <div class="col">
-        <router-link :to="`/usuario/${review.user}`" class="text-white fw-bold">
-          <Title tag="h4">@{{ review.user }}</Title>
+        <router-link :to="`/usuario/${comment.user}`" class="text-white fw-bold">
+          <Title tag="h4">@{{ comment.user }}</Title>
         </router-link>
       </div>
 
       <div class="col d-flex">
-        <StarRating v-if="Number(review.rating)" :value="review.rating" class="ms-auto me-3" />
+        <StarRating :value="comment.rating" class="ms-auto me-3" />
 
         <InteractiveIcon title="Denunciar">
-          <i class="bi bi-flag text-danger small"></i>
+          <i class="bi bi-flag text-danger fs-6"></i>
         </InteractiveIcon>
       </div>
     </div>
     <div class="col-11 ms-auto">      
       <p class="fs-6 mb-0 pre comment-text">
-        {{ review.text }}
+        {{ comment.text }}
       </p>
     </div>
     <div class="d-flex mt-2">
-      <p class="ms-auto small text-white-50">Última modificação em {{ review.date }}</p>
+      <p class="ms-auto small text-white-50">Última modificação em {{ comment.date }}</p>
     </div>
 
   </article>
@@ -42,7 +42,7 @@ export default {
     InteractiveIcon
   },
   props: {
-    review: Object
+    comment: Object
   },
 }
 </script>
