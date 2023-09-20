50772aa26f23c37edebcab4a9b753dc83d471a55
Murilo Sopi <dev.murilosopi@gmail.com>
2023-09-20 06:22:07 -0300

    cria tabela p reações

--- database.sql
+++ database.sql
@@ -65,6 +65,14 @@ CREATE TABLE tbReacoes(
     descricao VARCHAR(50)
 );
 
+CREATE TABLE tbReacoesResenha(
+    resenha INT NOT NULL,
+    reacao INT NOT NULL,
+
+    FOREIGN KEY (reacao) REFERENCES tbReacoes (id),
+    FOREIGN KEY (resenha) REFERENCES tbResenhas (id)
+);
+
 CREATE TABLE tbResenhas (
     id INT PRIMARY KEY AUTO_INCREMENT,
     filme INT NOT NULL,
@@ -75,8 +83,7 @@ CREATE TABLE tbResenhas (
     dataHora DATETIME DEFAULT CURRENT_TIMESTAMP,
     reacao INT,
     FOREIGN KEY (usuario) REFERENCES tbUsuarios (id),
-    FOREIGN KEY (filme) REFERENCES tbFilmes (id),
-    FOREIGN KEY (reacao) REFERENCES tbReacoes (id),
+    FOREIGN KEY (filme) REFERENCES tbFilmes (id)
 );
 
 /*Chat: */
