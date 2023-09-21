8060e3154c9b0001308f614da8a5642251f2989d
Murilo Sopi <dev.murilosopi@gmail.com>
2023-09-21 17:33:04 -0300

    ajusta configurações para realizar requisição de login

--- src/pages/user/Login.vue
+++ src/pages/user/Login.vue
@@ -15,35 +15,36 @@
     </div>
 
           
-      <div class="row justify-content-center">
-        <div class="col-md-10 col-lg-7 col-xl-5">
-          <InputCustom>
-            <i class="bi bi-person-circle" slot="icon"></i>
-            <input type="text" placeholder="Usuário" slot="input" class="form-control">
-          </InputCustom>
-        </div>
-      </div>
-      <div class="row justify-content-center">
-        <div class="col-md-10 col-lg-7 col-xl-5">
-          <InputCustom>
-            <i class="bi bi-lock-fill" slot="icon"></i>
-            <input type="password" placeholder="Senha" slot="input" class="form-control">
-          </InputCustom>
-        </div>
-      </div>
-
-      <div class="row justify-content-center">
-        <div class="col-md-10 col-lg-7 col-xl-5">
-          <div class="d-flex">
-            <ButtonCustom class="me-2" @click.native="back">
-              Voltar
-            </ButtonCustom>
-            <ButtonCustom variant="azul" class="ms-2">
-              Entrar
-            </ButtonCustom>
+      <form @submit.prevent="login">
+        <div class="row justify-content-center">
+          <div class="col-md-10 col-lg-7 col-xl-5">
+            <InputCustom>
+              <i class="bi bi-person-circle" slot="icon"></i>
+              <input type="text" placeholder="Usuário" slot="input" class="form-control" v-model="username">
+            </InputCustom>
           </div>
         </div>
-      </div>
+        <div class="row justify-content-center">
+          <div class="col-md-10 col-lg-7 col-xl-5">
+            <InputCustom>
+              <i class="bi bi-lock-fill" slot="icon"></i>
+              <input type="password" placeholder="Senha" slot="input" class="form-control" v-model="password">
+            </InputCustom>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-md-10 col-lg-7 col-xl-5">
+            <div class="d-flex">
+              <ButtonCustom class="me-2" @click.native="back">
+                Voltar
+              </ButtonCustom>
+              <ButtonCustom variant="azul" class="ms-2">
+                Entrar
+              </ButtonCustom>
+            </div>
+          </div>
+        </div>
+      </form>
     
     </main>
 </template>
@@ -55,6 +56,7 @@ import Title from '@/components/Title.vue';
 import ButtonCustom from '@/components/ButtonCustom.vue';
 import router from '@/routes';
 import PageMixin from '@/mixins/PageMixin.js';
+import { mapActions } from 'vuex';
 
 export default {
   components: {
@@ -62,10 +64,20 @@ export default {
     Title,
     ButtonCustom
   },
+  data() {
+    return {
+      username: '',
+      password: '',
+    }
+  },
   mixins: [PageMixin],
   methods: {
+    ...mapActions('auth', ['doLogin']),
     back() {
       router.go(-1)
+    },
+    login() {
+      this.doLogin({ username: this.username, password: this.password });
     }
   },
   created() {
