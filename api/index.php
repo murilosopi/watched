13a64f069ed1c7573ab33eb9d28c68380d70aef8
Murilo Sopi <dev.murilosopi@gmail.com>
2023-09-26 13:53:54 -0300

    corrige response headers da api

--- api/index.php
+++ api/index.php
@@ -5,7 +5,7 @@
   require_once "./vendor/autoload.php";
 
   header("Access-Control-Allow-Origin: http://localhost:8080");
-  header("Access-Control-Allow-Headers: *");
+  header("Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE");
   header("Access-Control-Allow-Credentials: true");
   session_start();
   $route = new \App\Route;
