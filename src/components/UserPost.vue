7910430b9b91d2849ef430178101799dc600fe91
Murilo Sopi <88057828+murilosopi@users.noreply.github.com>
2023-10-06 13:48:12 -0300

    ajustes no componente de UserPost

--- src/components/UserPost.vue
+++ src/components/UserPost.vue
@@ -2,10 +2,7 @@
     <DarkBox class="px-md-4 px-5 py-4">
         <article class="userpost">
             <div class="row align-items-center mb-2">
-                <figure class="col-1 p-0 mb-0">
-                    <img class="user-photo w-100" src="/assets/img/users/default.svg"
-                        :alt="`Foto de perfil de @${userpost.user}`" />
-                </figure>
+                <UserAvatar :username="userpost.user" class="col-1 d-none d-sm-block p-0 mb-0"/>
                 <div class="col">
                     <router-link :to="`/usuario/${userpost.user}`" class="text-white fw-bold">
                         <Title tag="h4">@{{ userpost.user }}</Title>
@@ -18,22 +15,22 @@
                 </div>
             </div>
             <div class="text-wrap text-break row">
-                <p class="col-11 ms-auto fs-6 mb-0 pre userpost-text">
+                <p class="col-sm-11 ms-auto fs-6 mb-0 pre userpost-text">
                     {{ userpost.text }}
                 </p>
             </div>
             <div class="row mt-2">
-                <div class="col column-gap-5 d-flex justify-content-end">
+                <div class="col column-gap-3 d-flex justify-content-end">
                     <InteractiveIcon title="Upvote">
-                        <i class="bi bi-chevron-compact-up text-primary fs-6" @click="addUpvote"></i>
+                        <i class="bi bi-chevron-compact-up text-primary fs-4" @click="addUpvote"></i>
                         <p class="text-center small text-white">{{ counterUp }}</p>
                     </InteractiveIcon>
                     <InteractiveIcon title="Downvote">
-                        <i class="bi bi-chevron-compact-down text-danger fs-6" @click="addDownvote"></i>
+                        <i class="bi bi-chevron-compact-down text-danger fs-4" @click="addDownvote"></i>
                         <p class="text-center small text-white">{{ counterDown }}</p>
                     </InteractiveIcon>
                     <InteractiveIcon title="DoComment">
-                        <i class="bi bi-chat text-white fs-6"></i>
+                        <i class="bi bi-chat text-white fs-5"></i>
                         <p class="text-center small text-white">{{ counterComment }}</p>
                     </InteractiveIcon>
                 </div>
@@ -45,6 +42,7 @@
 import Title from "./Title.vue";
 import InteractiveIcon from "./InteractiveIcon.vue";
 import DarkBox from "./DarkBox.vue";
+import UserAvatar from "./UserAvatar.vue";
 
 export default {
 
@@ -58,7 +56,8 @@ export default {
     components: {
         Title,
         InteractiveIcon,
-        DarkBox
+        DarkBox,
+        UserAvatar
     },
     props: {
         userpost: Object,
@@ -66,9 +65,9 @@ export default {
     methods: {
         reportPost() {
         },
-        addUpvote: function () {
+        addUpvote() {
         },
-        addDownvote: function () {
+        addDownvote() {
         },
     }
 }
