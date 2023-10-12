d048952442de097b08c83c8993b1ad18a5163d79
Murilo Sopi <dev.murilosopi@gmail.com>
2023-10-11 22:25:08 -0300

    registra alterações de curtida/salvo/assistido

--- src/mixins/MovieInteractionMixin.js
+++ src/mixins/MovieInteractionMixin.js
@@ -14,8 +14,6 @@ export default {
             this.saved = dados.salvo || false;
             this.liked = dados.curtido || false;
 
-            console.log(dados.curtido || false)
-      
             this.totalLiked = dados.totalCurtido;
             this.totalWatched = dados.totalAssistido;
             this.totalSaved = dados.totalSalvo;
@@ -26,6 +24,36 @@ export default {
     captureLiked() {
       if (this.userLogged) {
         this.liked = !this.liked;
+
+        if (this.liked) this.totalLiked++;
+        else this.totalLiked--;
+
+        const params = {
+          filme: this.id,
+        };
+
+        this.$api
+          .post("/alterar-curtida-filme", params)
+          .then((res) => {
+            const response = res.data;
+
+            if (!response.sucesso) {
+              this.liked = !this.liked;
+
+              if (this.liked) this.totalLiked++;
+              else this.totalLiked--;
+
+              throw response;
+            }
+          })
+          .catch(() => {
+            this.notifyUser({
+              icon: "x-circle",
+              title: "Ops",
+              text: "não foi possível registrar sua curtida...",
+              class: "danger",
+            });
+          });
       } else {
         this.notifyAuthRequired();
       }
@@ -33,6 +61,36 @@ export default {
     captureWatched() {
       if (this.userLogged) {
         this.watched = !this.watched;
+
+        if (this.watched) this.totalWatched++;
+        else this.totalWatched--;
+
+        const params = {
+          filme: this.id,
+        };
+
+        this.$api
+          .post("/alterar-assistido-filme", params)
+          .then((res) => {
+            const response = res.data;
+
+            if (!response.sucesso) {
+              this.watched = !this.watched;
+
+              if (this.watched) this.totalWatched++;
+              else this.totalWatched--;
+
+              throw response;
+            }
+          })
+          .catch(() => {
+            this.notifyUser({
+              icon: "x-circle",
+              title: "Ops",
+              text: "não foi possível registrar sua interação...",
+              class: "danger",
+            });
+          });
       } else {
         this.notifyAuthRequired();
       }
@@ -40,6 +98,36 @@ export default {
     captureSaved() {
       if (this.userLogged) {
         this.saved = !this.saved;
+
+        if (this.saved) this.totalSaved++;
+        else this.totalSaved--;
+
+        const params = {
+          filme: this.id,
+        };
+
+        this.$api
+          .post("/alterar-salvo-filme", params)
+          .then((res) => {
+            const response = res.data;
+
+            if (!response.sucesso) {
+              this.saved = !this.saved;
+
+              if (this.saved) this.totalSaved++;
+              else this.totalSaved--;
+
+              throw response;
+            }
+          })
+          .catch(() => {
+            this.notifyUser({
+              icon: "x-circle",
+              title: "Ops",
+              text: "não foi possível registrar sua interação...",
+              class: "danger",
+            });
+          });
       } else {
         this.notifyAuthRequired();
       }
