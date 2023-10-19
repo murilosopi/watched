2bfbc82ddbe556348cf61aa2eaf171d868b75e44
murilosopi <88057828+murilosopi@users.noreply.github.com>
2023-10-19 15:18:32 -0300

    cria componente ResultGroup

---
+++ src/components/ResultGroup.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="result-group row gx-0">
+        <div class="col-1 result-group-title-wrapper" v-if="title">
+          <Title class="result-group-title display-4" tag="h2">{{ title }}</Title>
+        </div>
+        <Scroller class="mt-5 col">
+          <slot></slot>
+        </Scroller>
+      </div>
+</template>
+
+<script>
+import Scroller from '@/components/Scroller.vue';
+import Title from '@/components/Title.vue';
+export default {
+    components: {
+        Scroller,
+        Title
+    },
+    props: {
+      title: String
+    }
+}
+</script>
+
+<style scoped>
+.result-group-title-wrapper {
+  display: flex;
+  align-items: center;
+}
+
+.result-group-title {
+  writing-mode: vertical-rl;
+  text-orientation: sideways;
+  rotate: 180deg;
+  margin: 0 auto !important;
+  opacity: 0.5;
+}
+</style>
--- src/pages/search/Search.vue
+++ src/pages/search/Search.vue
@@ -6,28 +6,32 @@
 
       <hr />
 
-      <div class="row gx-0">
-        <div class="col-1 result-title-wrapper">
-          <Title class="result-title display-4" tag="h2"> Perfis </Title>
-        </div>
-        <Scroller class="mt-5 col">
-          <ul class="list-unstyled">
-            <li
-              class="col-md-3 col-sm-4 col-5"
-              v-for="user in users"
-              :key="user.id"
-            >
-              <router-link
-                :to="`/usuario/${user.tag}`"
-                class="text-light text-center"
-              >
-                <UserAvatar :username="user.tag" />
-                <h3 class="h6 text-uppercase mt-2">@{{ user.tag }}</h3>
-              </router-link>
+      <ResultGroup title="Perfis">
+        <ul class="list-unstyled">
+          <li class="col-md-3 col-sm-4 col-5" v-for="user in users" :key="user.id">
+            <router-link :to="`/usuario/${user.tag}`" class="text-light text-center">
+              <UserAvatar :username="user.tag" />
+              <Title class="h5 mt-2">@{{ user.tag }}</Title>
+            </router-link>
+          </li>
+
+          <template v-if="!users.length">
+            <li class="col-md-3 col-sm-4 col-5" v-for="i in 10" :key="i">
+              <UserAvatar username=""/>
+              <div class="row">
+                <div class="placeholder mt-3 py-3 rounded px-3 col-8 mx-auto"></div>
+              </div>
             </li>
-          </ul>
-        </Scroller>
-      </div>
+          </template>
+
+
+        </ul>
+      </ResultGroup>
+
+      <ResultGroup title="Filmes">
+        <MoviesPanel :movies="movies" :inline="true"/>
+      </ResultGroup>
+
     </div>
   </div>
 </template>
@@ -36,7 +40,8 @@
 import ResultFilter from "./ResultFilter.vue";
 import SortingMenu from "@/components/SortingMenu.vue";
 import UserAvatar from "@/components/UserAvatar.vue";
-import Scroller from "@/components/Scroller.vue";
+import ResultGroup from "@/components/ResultGroup.vue";
+import MoviesPanel from "@/components/MoviesPanel.vue";
 import Title from "@/components/Title.vue";
 
 export default {
@@ -44,44 +49,19 @@ export default {
     ResultFilter,
     SortingMenu,
     UserAvatar,
-    Scroller,
-    Title,
+    ResultGroup,
+    MoviesPanel,
+    Title
   },
   props: {
     search: String,
   },
   data() {
     return {
-      users: [
-        { tag: "kksopi", id: 1 },
-        { tag: "kksopi", id: 2 },
-        { tag: "kksopi", id: 3 },
-        { tag: "kksopi", id: 4 },
-        { tag: "kksopi", id: 5 },
-        { tag: "kksopi", id: 6 },
-        { tag: "kksopi", id: 7 },
-        { tag: "kksopi", id: 8 },
-        { tag: "kksopi", id: 9 },
-        { tag: "kksopi", id: 10 },
-        { tag: "kksopi", id: 11 },
-        { tag: "kksopi", id: 12 },
-      ],
+      users: [],
+
+      movies: [],
     };
   },
 };
 </script>
-
-<style scoped>
-.result-title-wrapper {
-  display: flex;
-  align-items: center;
-}
-
-.result-title {
-  writing-mode: vertical-rl;
-  text-orientation: sideways;
-  rotate: 180deg;
-  margin: 0 auto !important;
-  opacity: 0.5;
-}
-</style>
